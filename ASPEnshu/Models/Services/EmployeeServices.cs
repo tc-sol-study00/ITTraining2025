e0dd47fc83dc670a1faf7950b6d2134c317303dd
Shigeyuki Too <shigeyuki.to@techno-creative.co.jp>
2025-09-03 13:32:39 +0900

    メソッドの整理

--- ASPEnshu/Models/Services/EmployeeServices.cs
+++ ASPEnshu/Models/Services/EmployeeServices.cs
@@ -11,6 +11,10 @@ namespace ASPEnshu.Models.Services {
             _context = context;
         }
 
+        /*
+         * 取り出し系
+         */
+
         /// <summary>
         /// Employee全行取り出し
         /// </summary>
@@ -18,28 +22,6 @@ namespace ASPEnshu.Models.Services {
         public async Task<List<Employee>> GetAllEmployeesAsync() =>
             await _context.Employee.ToListAsync();
 
-        /// <summary>
-        /// Employeeテーブルのデータ存在チェック
-        /// </summary>
-        /// <returns>存在していればtrueしていなければfalse</returns>
-        private async Task<bool> EmployeeExistCheckAsync() =>
-            _context.Employee == null ? false : true;
-
-        /// <summary>
-        /// 主キー(id)指定された場合のEmployee取り出し（１行）
-        /// </summary>
-        /// <param name="id">主キー</param>
-        /// <returns>Employee（１行）</returns>
-        public async Task<Employee?> GetEmployeeByIdAsync(string id) =>
-            await _context.Employee.FirstOrDefaultAsync(m => m.EmployeeNo == id);
-
-        /// <summary>
-        /// 主キーに該当するEmployeeの存在チェック
-        /// </summary>
-        /// <param name="id">主キー</param>
-        /// <returns>存在していればtrue・なければfalse</returns>
-        private async Task<bool> EmployeeExistsAsync(string id) =>
-            await (_context.Employee?.AnyAsync(e => e.EmployeeNo == id) ?? Task.FromResult(false));
 
         /// <summary>
         /// 主キーに該当するEmployeeの存在チェックを行い、
@@ -48,7 +30,7 @@ namespace ASPEnshu.Models.Services {
         /// <param name="id"></param>
         /// <returns>存在していればEmployee(一行）を返却、なければnullを返却</returns>
         public async Task<Employee?> GetEmployeeWithExistCheck(string id) {
-            if (id == null || !await EmployeeExistCheckAsync()) {
+            if (id == null || _context.Employee == null) {
                 return null;
             }
 
@@ -56,10 +38,19 @@ namespace ASPEnshu.Models.Services {
             if (employee == null) {
                 return null;
             }
-
             return employee;
-
         }
+        /// <summary>
+        /// 主キー(id)指定された場合のEmployee取り出し（１行）
+        /// </summary>
+        /// <param name="id">主キー</param>
+        /// <returns>Employee（１行）</returns>
+        private async Task<Employee?> GetEmployeeByIdAsync(string id) =>
+            await _context.Employee.FirstOrDefaultAsync(m => m.EmployeeNo == id);
+
+        /*
+         * 更新系
+         */
         /// <summary>
         /// Employeeデータ追加
         /// </summary>
@@ -76,20 +67,21 @@ namespace ASPEnshu.Models.Services {
         /// <param name="employee"></param>
         /// <returns></returns>
         public async Task<bool> UpdateEmployeeAsync(Employee employee) {
-
             try {
                 _context.Update(employee);
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException) {
-                if (!await EmployeeExistsAsync(employee.EmployeeNo)) {
+
+                bool checkResult=
+                    await (_context.Employee?.AnyAsync(e => e.EmployeeNo == employee.EmployeeNo) ?? Task.FromResult(false));
+                if (checkResult) {
                     return false;
                 }
                 else {
                     throw;
                 }
             }
-
             return true;
         }
 
@@ -100,9 +92,10 @@ namespace ASPEnshu.Models.Services {
         /// <returns></returns>
         public async Task<bool> RemoveEmployeeAsync(string id) {
 
-            if (!await EmployeeExistCheckAsync()) {
+            if (_context.Employee == null) {
                 return false;
             }
+
             var employee = await GetEmployeeByIdAsync(id);
             if (employee != null) {
                 _context.Employee.Remove(employee);
