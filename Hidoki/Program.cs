86cef9065096183c6d5615a8f5a22dadc35e9d66
Shigeyuki Too <shigeyuki.to@techno-creative.co.jp>
2025-09-03 16:41:29 +0900

    非同期サンプル追加

---
+++ Hidoki/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+
+namespace Hidoki {
+    internal class Program {
+        static async Task Main(string[] args) {
+            Hidoki hidoki = new Hidoki();
+            Task<int> task = hidoki.HidokiMethod();
+
+            int ttl = 0;
+            for (int i = 0; i < 10000; i++) ttl += i;
+
+            int result = await task;
+
+            ttl += result;
+
+            Console.WriteLine(ttl);
+        }
+
+
+    }
+
+    public class Hidoki {
+        public async Task<int> HidokiMethod() {
+            await Task.Delay(10000);
+
+            return 1;
+        }
+    }
+}
